c85ba6fa56b76074eda7c0dbf844edb43955335a
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-08-24 20:40:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-    <h1 data-ns-test = "Rendering Multiple Components with React">Rendering Multiple Components with React</h1>
+    <h1 data-ns-test = "myprojectt">My Project</h1>
+    <p data-ns-test = "description">This is my first project</p>
     </div>
   )
 }
